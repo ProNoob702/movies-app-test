ae464604e8363b451180849b7090597f8101d282
taha.seddik1992@gmail.com <taha.seddik1992@gmail.com>
2022-09-23 07:31:48 +0100

    layout setup + font awesome

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,14 @@
-import logo from "./logo.svg";
 import "./App.scss";
+import "react-toastify/dist/ReactToastify.css";
+import { AppLayout } from "./layout/layout";
+import { ToastContainer } from "react-toastify";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppLayout>
+      <div>zouta</div>
+      <ToastContainer />
+    </AppLayout>
   );
 }
 
---
+++ src/components/shared/btn.component.tsx
@@ -0,0 +1,22 @@
+interface IProps {
+  bgColor: string;
+  textColor?: string;
+  label: string;
+  classname?: string;
+  onClick: (...args: any[]) => any;
+}
+
+export const NiceBtn: React.FC<IProps & any> = ({ bgColor, textColor, label, classname, onClick }) => {
+  const finalTextColor = textColor ? textColor : "black";
+  return (
+    <button
+      className={`hover:bg-${bgColor}-600 ${classname} text-${finalTextColor}-600 border border-${bgColor}-600 hover:text-white
+         active:bg-${bgColor}-600 font-bold uppercase text-sm px-6 py-3 rounded
+          outline-none focus:outline-none mb-1 ease-linear transition-all duration-150 `}
+      type="button"
+      onClick={onClick}
+    >
+      {label}
+    </button>
+  );
+};

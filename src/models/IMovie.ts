85bf15ab53b760749285830bf976a6bde397ccaa
taha.seddik1992@gmail.com <taha.seddik1992@gmail.com>
2022-09-23 20:49:51 +0100

    moviesData

--- src/models/IMovie.ts
+++ src/models/IMovie.ts
@@ -1,3 +1,17 @@
+export type MovieGenre = "action" | "adventure" | "drama" | "fantasy";
+
 export interface IMovie {
+  id: string;
   name: string;
+  genre: MovieGenre[];
+  description: string;
+  imgUrl: string;
 }
+
+//   action = "action" ,
+//   adventure =  "adventure",
+//   comedy =   "comedy",
+//   drama = "drama",
+//   fantasy = "fantasy",
+//   horror = "horror",
+// }

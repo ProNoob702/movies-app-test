f9dd77d822a30d2086f10c7afceb6ed54e00ed25
taha.seddik1992@gmail.com <taha.seddik1992@gmail.com>
2022-09-23 07:55:03 +0100

    temporay kick routes

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,12 @@ import "./App.scss";
 import "react-toastify/dist/ReactToastify.css";
 import { AppLayout } from "./layout/Layout";
 import { ToastContainer } from "react-toastify";
+import { AppRoutes } from "./routes/Routes";
 
 function App() {
   return (
     <AppLayout>
-      <div>zouta</div>
+      <AppRoutes />
       <ToastContainer />
     </AppLayout>
   );

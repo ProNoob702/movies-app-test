1cac1bf94a4c20a17f5a27f02bbd93877dbd7c93
taha.seddik1992@gmail.com <taha.seddik1992@gmail.com>
2022-09-24 03:55:02 +0100

    move search bar to new file

--- src/components/moviesList/MoviesList.tsx
+++ src/components/moviesList/MoviesList.tsx
@@ -1,12 +1,9 @@
-import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
-import { useDebounce } from "../../hooks/useDebounce";
 import { getMoviesList } from "../../redux-store/selectors/movies.selectors";
-import { appActions, useAppSelector } from "../../redux-store/store";
+import { useAppSelector } from "../../redux-store/store";
 import { CardComponent } from "../shared/card.component";
 import "./MoviesList.scss";
+import { SearchBar } from "./SearchBar";
 
 export const MoviesList: React.FC<{}> = () => {
   return (
@@ -14,38 +11,6 @@ export const MoviesList: React.FC<{}> = () => {
       <SearchBar />
       <MoviesZone />
     </>
-  );
-};
-
-const SearchBar: React.FC<{}> = () => {
-  const dispatch = useDispatch();
-
-  const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
-    e.preventDefault();
-    dispatch(appActions.doSearchForMovies(e.target.value));
-  };
-
-  const debouncedHandleSearch = useDebounce(handleSearch, 500);
-
-  return (
-    <form className="w-1/2 m-auto">
-      <label htmlFor="default-search" className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-gray-300">
-        Search
-      </label>
-      <div className="relative">
-        <div className="flex absolute inset-y-0 left-0 items-center pl-4 pointer-events-none">
-          <FontAwesomeIcon className="w-4 h-4 text-gray-500" icon={faMagnifyingGlass} />
-        </div>
-        <input
-          type="search"
-          id="default-search"
-          className="custom-search-input"
-          placeholder="Search Movies..."
-          onChange={debouncedHandleSearch}
-          required
-        />
-      </div>
-    </form>
   );
 };
 
